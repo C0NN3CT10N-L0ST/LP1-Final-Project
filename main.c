48c1dbfd68fea24a0d76617c2180de30081abfb3
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-06-17 23:38:44 +0100

    Implements 'initializeCellsList' and 'insertBoardCell' functions

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "board.h"
 
 
 #define MAX_CELLS 128  // Defines the max number of cells that can exist in the board
 
 /* Program Functions' Signature */
-void getSafeCellsFromConfigFile(char *fileName, int *safeCells);
+
+int getSafeCellsFromConfigFile(char *fileName, int *safeCells);
+void initializeCellsList(list *boardCells);
+int insertBoardCell(list *boardCells, node cell);
+node cell;
 
 
 int main(int argc, char const *argv[])
@@ -13,31 +19,51 @@ int main(int argc, char const *argv[])
     /* Program variables */
     // Default values for 'Board Presentation Mode', 'Number of Lines' and 'Number of Columns'
     int boardPresentation = 0, linesNum = 3, columnsNum = 7;
-    int safeCells[MAX_CELLS];  // Stores the safe cells read from the config file
-    
-    
+    int safeCells[MAX_CELLS] = {0};  // Stores the safe cells read from the config file
+    int totalCells;  // Number of total cells
+    list boardCells;  // List struct to store all board cells data
+
+    // Initializes random seed
+    srand(1);
+
+
     // Gets program args and checks if they're valid
     for (int i = 1; i < argc; i++) {
         // Checks if 'Board Presentation Mode' argument is set and valid
-        if (i == 1 && (argv[i] == 0 || argv[i] == 1)) {
-            boardPresentation = argv[i];
+        if (i == 1 && (atoi(argv[i]) == 0 || atoi(argv[i]) == 1)) {
+            boardPresentation = atoi(argv[i]);
         }
 
         // Checks if the 'Number of lines' is set and valid
-        if (i == 2 && argv[i] >= MIN_ROWS && (int)argv[i] % 2 != 0) {
-            linesNum = argv[i];
+        if (i == 2 && atoi(argv[i]) >= MIN_ROWS && atoi(argv[i]) % 2 != 0) {
+            linesNum = atoi(argv[i]);
         }
 
         // Checks if the 'Number of Columns' is set and valid
-        if (i == 3 && argv[i] > MIN_COLS) {
-            columnsNum = argv[i];
+        if (i == 3 && atoi(argv[i]) > MIN_COLS) {
+            columnsNum = atoi(argv[i]);
         }
 
         // Checks if the 'Configuration File' is present and reads its content
         if (i == 4) {
             // Reads safe cells from config file and stores them in an array
-            getSafeCellsFromConfigFile(argv[i], &safeCells);
+            int getSafeCells = getSafeCellsFromConfigFile((char*)argv[i], safeCells);
+
+            // Exits program if it fails to get safe cells from the config file
+            if (getSafeCells == 1) {
+                return 0;
+            }
         }
+    }
+
+    // Updates the number of total cells based on given arguments
+    totalCells = linesNum * 2 + (columnsNum - 2) * 2;
+
+    // Initializes the board cells list
+    initializeCellsList(&boardCells);
+
+    for (int cell = 0; cell < totalCells; cell++) {
+        printf("Number in pos %d: %d\n", cell, safeCells[cell]);
     }
     
     
@@ -46,7 +72,14 @@ int main(int argc, char const *argv[])
 
 
 /* Program Functions */
-void getSafeCellsFromConfigFile(char *fileName, int *safeCells) {
+
+/**
+ * @brief Gets the board safe cells from the config file given as a program argument
+ * @param fileName The name of the config file
+ * @param safeCells An array which stores all the safe cells read from the config file
+ * @return Returns 0 on 'SUCCESS' and 1 on 'FAILURE'
+*/
+int getSafeCellsFromConfigFile(char *fileName, int *safeCells) {
     FILE *fp;
     int cellIndex = 0;
     int numberRead;
@@ -56,8 +89,8 @@ void getSafeCellsFromConfigFile(char *fileName, int *safeCells) {
 
     // Checks for file read errors
     if (fp == NULL) {
-        fputs(stdout, FILE_ERR1);
-        exit(0);
+        fputs(FILE_ERR1, stdout);
+        return 1;
     }
 
     // Reads all numbers from config file and stores them in 'safeCells'
@@ -68,4 +101,45 @@ void getSafeCellsFromConfigFile(char *fileName, int *safeCells) {
 
     // Closes file stream
     fclose(fp);
+    return 0;
+}
+
+/**
+ * @brief Initializes the board cells' list
+ * @param boardCells The list that holds the board cells
+ */
+void initializeCellsList(list *boardCells) {
+    boardCells->head = NULL;
+    boardCells->tail = NULL;
+    boardCells->length = 0;
+}
+
+/**
+ * @brief Inserts a new board cell into the board cells' linked list.
+ * @param boardCells The board cells list
+ * @param cell The cell to be inserted
+ * @return Returns 0 on 'SUCCESS' and 1 on 'FAILURE'
+ */
+int insertBoardCell(list *boardCells, node cell) {
+    // Creates a new node
+    node *boardCell = (node*) malloc(sizeof(node));
+
+    // Checks if an ERROR ocurred (i.e. out of memory)
+    if (boardCell == NULL) return 1;
+
+    // Adds cell data from the given cell to the new cell pointer
+    boardCell->item = cell.item;
+    boardCell->next = NULL;
+    
+    if (boardCells->head == NULL) {  // List is empty
+        boardCells->head = boardCell;
+        boardCells->tail = boardCell;
+        boardCells->length = 1;
+    } else {  // List has more than 1 item
+        boardCells->tail->next = boardCell;
+        boardCells->tail = boardCell;
+        boardCells->length++;
+    }
+
+    return 0;
 }
